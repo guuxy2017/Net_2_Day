71cd6f991c81526d3dbf0f8d14768e75d3982eea
guuxy2017 <guuxy@163.com>
2018-04-15 22:25:15 +0800

    20180415 第一次提交

--- 12 三个练习/Program.cs
+++ 12 三个练习/Program.cs
@@ -36,33 +36,113 @@ namespace _12_三个练习
             //     {
             //         Console.WriteLine("错误密码");
             //     }
-
             //}
+            //Console.ReadKey();
             #endregion
 
-            Console.WriteLine("请输入密码");
-            string pwd = Console.ReadLine();
+            #region   赵老师做的
+            //Console.WriteLine("请输入密码");
+            //string pwd = Console.ReadLine();
 
-            if (pwd == "888888")
+            //if (pwd == "888888")
+            //{
+            //    Console.WriteLine("密码正确");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("密码错误，请再次输入密码");
+            //    pwd = Console.ReadLine();
+            //    if (pwd == "888888")
+            //    {
+            //        Console.WriteLine("密码正确");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("白痴，2次都输错密码，程序退出");
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            ////  提示用户输入用户名，然后再提示输入密码，
+            ////如果用户名是“admin”并且密码是“88888”，则提示正确，
+            ////否则，如果用户名不是admin还提示用户用户名不存在,
+            ////如果用户名是admin则提示密码错误.
+            //Console.WriteLine("请输入用户名");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("请输入密码");
+            //string pwd = Console.ReadLine();
+
+            #region  自己做
+            ////if (name == "admin")
+            ////{
+            ////    if (pwd == "8888")
+            ////    {
+            ////        Console.WriteLine("输入正确");
+            ////    }
+            ////    else
+            ////    {
+            ////        Console.WriteLine("密码错误");
+            ////    }
+            ////}
+            ////else 
+            ////{
+            ////    Console.WriteLine("用户名不存在");
+            ////}
+            #endregion
+
+            #region  老师做
+            ////if (name == "admin" && pwd =="888888")
+            ////{
+            ////     Console.WriteLine("输入正确");
+            ////}
+            ////else if (name == "admin")
+            ////{
+            ////     Console.WriteLine("密码错误");
+            ////}
+            ////else if (pwd == "888888")
+            ////{
+            ////    Console.WriteLine("用户名错误");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("未知错误");
+            ////}
+            #endregion
+            //Console.ReadKey();
+
+            //练习3：提示用户输入年龄，如果大于等于18，则告知用户可以查看，
+            //如果小于10岁，则告知不允许查看，如果大于等于10岁并且小于18，
+            //则提示用户是否继续查看（yes、no），如果输入的是yes则提示用户请查看，
+            //否则提示"退出,你放弃查看"。
+            Console.WriteLine("请输入您的年龄！");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            #region  自己做的方法
+            if (age < 10)
             {
-                Console.WriteLine("密码正确");
+                Console.WriteLine("年龄小，不能看");
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("可以查看");
             }
             else
             {
-                Console.WriteLine("密码错误，请再次输入密码");
-                pwd = Console.ReadLine();
-                if (pwd == "888888")
+                Console.WriteLine("是否继续查看?（yes、no）");
+                string input = Console.ReadLine();
+                if (input == "yes")
                 {
-                    Console.WriteLine("密码正确");
+                    Console.WriteLine("请查看");
                 }
-                else
+                if (input == "no")
                 {
-                    Console.WriteLine("白痴，2次都输错密码，程序退出");
+                    Console.WriteLine("退出,你放弃查看");
                 }
             }
+            #endregion
 
             Console.ReadKey();
-
         }
     }
 }
---
+++ 13 Switch_case/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _13_Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //李四的年终工作评定,如果定为A级,则工资涨500元,
+            //如果定为B级,则工资涨200元,
+            //如果定为C级,工资不变,
+            //如果定为D级工资降200元,
+            //如果定为E级工资降500元.
+            //设李四的原工资为5000,请用户输入李四的评级,然后显示李四来年的工资.
+
+            #region    if else-if方法
+            //Console.WriteLine("请输入李四的工作评定：A、B、C、D、E");
+            //string   level = Console.ReadLine();  // A  B  C  D  E  其他输入
+            //double salary =5000;   //声明salary变量double类型，并赋值5000
+            //bool b = true;   //设置标记
+ 
+            //if (level =="A")
+            //{
+            //    salary += 500;  //salary=salary + 500
+            //}
+            //else if (level =="B")
+            //{
+            //    salary += 200;
+            //}
+            //else if (level =="C")
+            //{
+            //                        //不写，代表：salary += 0;
+            //}
+            //else if (level =="D")
+            //{
+            //    salary -= 200;
+            //}
+            //else if (level == "E")
+            //{
+            //    salary -= 500;
+            //}
+            //else      //输入错误
+            //{
+            //    Console.WriteLine("输入有误，程序退出！");
+            //    b = false;
+            //}
+            //if (b==true )
+            //{
+            //        Console.WriteLine("李四的年终工资是{0}",salary );
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region  Switch-case方法
+            bool b = true;
+            Console.WriteLine("请输入李四的年终评定：A  B  C  D  E");
+            string level = Console.ReadLine();
+            double salary = 5000;
+
+            switch (level )
+            {
+                case "A": salary += 500;
+                    break;
+                case "B": salary += 200;
+                    break;
+                case "C": break;
+                case "D": salary -= 200;
+                    break;
+                case "E": salary -= 500;
+                    break;
+                default: Console.WriteLine("输入错误，程序退出！");
+                    b = false;
+                    break;
+            }
+
+            if (b==true)
+            {
+                 Console.WriteLine("李四明年的工资 是{0}",salary );
+            }
+           
+            Console.ReadKey();
+            #endregion
+
+
+        }
+    }
+}
